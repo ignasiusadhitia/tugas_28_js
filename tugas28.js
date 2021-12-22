2cca33cdf30aaa94925c61bbb3b13c5f4f8a1732
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-22 13:11:07 +0700

    Pengumpulan Tugas 28 JS - NIOMIC

---
+++ tugas28.js
@@ -0,0 +1,8 @@
+console.log("REGEX\n======================================");
+
+function regex() {
+  let str = "abcdefghijklmnopqrstuvwxyz";
+
+  console.log("Urut ke:", String(str.search(/s/)));
+}
+regex();
